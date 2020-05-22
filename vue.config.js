9bdfb092220c2c390e1bc0022cf510df1eafa0ef
hsf <shuifa.huang@keepsoft.net>
2020-05-22 14:18:58 +0800

    [edit]rm eslint

--- vue.config.js
+++ vue.config.js
@@ -18,7 +18,7 @@ module.exports = {
   //放置生成的静态资源 (js、css、img、fonts) 的 (相对于 outputDir 的) 目录
   assetsDir: "assets",
   // eslint-loader 是否在保存的时候检查 安装@vue/cli-plugin-eslint有效
-  lintOnSave: true,
+  lintOnSave: false,
   //是否使用包含运行时编译器的 Vue 构建版本。设置true后你就可以在使用template
   runtimeCompiler: true,
   // 生产环境是否生成 sourceMap 文件 sourceMap的详解请看末尾
@@ -87,11 +87,6 @@ module.exports = {
         target: "xxx",
       },
     },
-    overlay: {
-      warnings: false,
-      errors: false,
-    },
-    lintOnSave: false,
   },
   pluginOptions: {
     // 第三方插件配置
