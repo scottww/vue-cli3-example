b6a84758b0650de19a88f1102e837ff49cc4eecf
scottww <798868678@qq.com>
2020-07-04 23:55:58 +0800

    [edit]makeLine, compute maxX、Y，gapX、Y

--- src/views/echarts/Demo1.vue
+++ src/views/echarts/Demo1.vue
@@ -22,33 +22,77 @@ export default {
       this.myChart = this.$echarts.init(this.$refs.myChart);
       this.myChart.clear();
 
+      let xMin = 0,
+        yMin = 0,
+        xMax = 80,
+        yMax = 100;
+
+      let markLineOpt = {
+        animation: false,
+        silent: true,
+        symbol: ["none", "arrow"],
+        // data: [
+        //   {
+        //     type: "average"
+        //   }
+        // ],
+        data: [[{ coord: [xMin, yMin] }, { coord: [xMax, yMax] }]],
+        lineStyle: {
+          show: true,
+          color: "#c5c5c5",
+          type: "dashed"
+        }
+      };
+
+      let defaultMaxX = 20,
+        defaultGapX = 2,
+        defaultMaxY = 100,
+        defaultGapY = 10;
+
       const options = {
         color: ["#5b9bd5", "#ed7d31"],
         legend: {
           data: ["Pa0", "Pa1"]
         },
+        // tooltip: {
+        // trigger: "axis",
+        // axisPointer: {
+        //   type: "cross"
+        // }
+        // },
         grid: {
+          top: "15%",
           left: "10%",
-          right: "5%",
+          right: "15%",
           bottom: "10%",
           containLabel: true
         },
         xAxis: {
           name: "P(mm)",
-          nameLocation: "middle",
-          nameGap: 40
+          max: Math.ceil(77 / defaultMaxX) * defaultMaxX,
+          interval: Math.ceil(77 / defaultMaxX) * defaultGapX,
+          splitLine: {
+            lineStyle: "none"
+          },
+          nameLocation: "end",
+          nameTextStyle: {
+            padding: [70, 0, 0, -45]
+          }
         },
         yAxis: {
           //type: category | value
           name: "R(mm)",
-          nameLocation: "middle",
-          nameRotate: 90,
-          nameGap: 40,
+          max: Math.ceil(44 / defaultMaxY) * defaultMaxY,
+          interval: Math.ceil(44 / defaultMaxY) * defaultGapY,
           axisLabel: {
             formatter: function(v) {
               return v;
             }
-          }
+          },
+          splitLine: {
+            lineStyle: "none"
+          },
+          nameLocation: "end"
         },
         series: [
           // ["0", "1", "2", "1", "4"]
@@ -60,10 +104,10 @@ export default {
             name: "Pa0",
             data: [
               [0, 0],
-              [66, 1],
-              [33, 2],
-              [55, 1],
-              [22, 4]
+              [66, 11],
+              [33, 22],
+              [55, 11],
+              [22, 44]
             ],
             type: "line",
             smooth: true,
@@ -72,7 +116,8 @@ export default {
               shadowColor: "rgba(0,0,0,0.4)",
               shadowBlur: 10,
               shadowOffsetY: 10
-            }
+            },
+            markLine: markLineOpt
           },
           {
             symbolSize: 10,
@@ -80,10 +125,10 @@ export default {
             name: "Pa1",
             data: [
               [0, 0],
-              [77, 1],
-              [44, 2],
-              [66, 1],
-              [33, 4]
+              [77, 11],
+              [44, 22],
+              [66, 11],
+              [33, 44]
             ],
             type: "line",
             smooth: true,
@@ -92,7 +137,8 @@ export default {
               shadowColor: "rgba(0,0,0,0.4)",
               shadowBlur: 10,
               shadowOffsetY: 10
-            }
+            },
+            markLine: markLineOpt
           }
         ]
       };
